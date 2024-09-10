22f1f4bf26aaef75f41c98c257bf9b50c752d497
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2024-09-10 19:28:45 -0300

    atividade2

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -3,27 +3,17 @@ import java.util.Scanner;
 public class App {
         //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in);
+public static void main(String[] args){
+        // Criando uma instância da classe RA2920482411036
 
-        //variável local 
-        String v2;
-        int v1;
+        RA2920482411036 aluno = new RA2920482411036();
 
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
-    }
+        System.out.println("Nome Completo: " + aluno.nome);
+        System.out.println("RA: " + aluno.ra);
+        System.out.println("Idade: " + aluno.idade);
+        System.out.println("GitHub: " + aluno.github);
+        System.out.println("LinkedIn: " + aluno.linkedIn);
+        System.out.println("Curso: " + aluno.curso);
+        System.out.println("Turma: " + aluno.turma);
+        }
 }
---
+++ FazerVsCode/src/RA2920482411036.java
@@ -0,0 +1,12 @@
+public class RA2920482411036 {
+    
+    public  String nome = "Ana Caroline Santana Moraes";
+    public  String ra = "2920482411036";
+    public  int idade = 18;
+    public  String github = "https://github.com/Anymoraes5";
+    public  String linkedIn = "https://www.linkedin.com/in/any-moraes-86a815292";
+    public  String turma = "2ADS";
+    public  String curso = "Análise e desenvolvimento de sistemas";
+
+    
+}
